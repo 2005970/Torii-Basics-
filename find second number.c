7ed384935eb9b25c9613c39d96b3784b672f2b26
chandana <chandana4871@gmail.com>
2025-11-04 13:25:41 +0530

    Create find second number.c

---
+++ find second number.c
@@ -0,0 +1,9 @@
+//The average of two numbers is X. The first number is Y. Find the second number.
+
+#include<stdio.h>
+int main()
+{
+int x,y;
+scanf("%d %d",&x,&y);
+int z=(2*x)-y;
+printf("%d",z);
