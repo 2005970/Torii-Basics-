77b5b4e49cdfd3d80b24fd24c6014496486b617e
chandana <chandana4871@gmail.com>
2025-11-04 21:30:18 +0530

    Create Even or Odd.c
    Find out whether the given number is Even or Odd.

---
+++ Even or Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int X;
+    scanf("%d", &X);
+    if (X % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
